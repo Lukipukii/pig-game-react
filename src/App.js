ae9230a8d117f726db5e1a40b865d716ee677691
Lucas Colban <lukipuki.git@gmail.com>
2024-02-09 14:32:52 +0100

    Creadas variables de estado useState y usarlas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import './App.css'
 import Player from './Player/Index'
+import { useState } from 'react'
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState(0)
+  const [score, setScore] = useState([0, 0])
+  const [current, setCurrent] = useState(0)
+  const [diceNumber, setDiceNumber] = useState(3)
+
+  const handleNewGame = () => {
+    activePlayer = true
+    score = 0
+    current = 0
+  }
+
+  const handleRollDice = () => rollDice()
+
+  const handleHold = () => {}
+
+  const rollDice = () => {
+    const num = Math.trunc(Math.random() * 6) + 1
+    return '../public/dice-{`num`}.png'
+  }
+
   return (
     <main>
-      <Player name={'Player 1'} score={43} current={0} isActive={true} />
-      <Player name={'Player 2'} score={24} current={0} isActive={false} />
-
-      <img src="dice-5.png" alt="Playing dice" className="dice" />
-      <button className="btn btn--new">🔄 New game</button>
-      <button className="btn btn--roll">🎲 Roll dice</button>
-      <button className="btn btn--hold">📥 Hold</button>
+      <Player
+        name={'Player 1'}
+        score={score[0]}
+        current={activePlayer === 1 && current}
+        isActive={activePlayer === 1}
+      />
+      <Player
+        name={'Player 2'}
+        score={score[1]}
+        current={activePlayer === 2 && current}
+        isActive={activePlayer === 2}
+      />
+      {diceNumber && (
+        <img
+          src={`dice-${diceNumber}.png`}
+          alt="Playing dice"
+          className="dice"
+        />
+      )}
+      <button className="btn btn--new" onClick={handleNewGame}>
+        🔄 New game
+      </button>
+      <button className="btn btn--roll" onClick={handleRollDice}>
+        🎲 Roll dice
+      </button>
+      <button className="btn btn--hold" onClick={handleHold}>
+        📥 Hold
+      </button>
     </main>
   )
 }
